82771972985a7920cf6fb4073d3d998f231fbe1a
Asus Ryzen 5 <esmeralda298rivera@gmail.com>
2025-05-07 22:46:17 -0600

    commit: restablecer angular.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "../componentes/navbar/navbar/navbar.component";
+import { HeaderComponent } from "../componentes/header/header/header.component";
+import { FooterComponent } from "../componentes/footer/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
