75755677e95b7e546d9ba66979519c3c22a929b3
Asus Ryzen 5 <esmeralda298rivera@gmail.com>
2025-05-12 12:04:50 -0600

    Commit : modificaciones en el sevicio y en el catalogo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { InicioComponent } from '../componentes/inicio/inicio.component';
 import { LowerCasePipe } from '@angular/common';
 import { LogInComponent } from '../componentes/log-in/log-in.component';
 import { CatalogoComponent } from '../componentes/catalogo/catalogo.component';
-import { DetalleAutoComponent } from '../componentes/detalle-auto/detalle-auto.component';
+//import { DetalleAutoComponent } from '../componentes/detalle-auto/detalle-auto.component';
 import { ServiciosComponent } from '../componentes/servicios/servicios/servicios.component';
 import { RegisterComponent } from '../componentes/register/register.component';
 import { FinanciamientoComponent } from '../componentes/financiamiento/financiamiento.component';
@@ -14,7 +14,7 @@ export const routes: Routes = [
     {path:'log-in',component:LogInComponent},
     {path:'catalogo',component:CatalogoComponent},
     {path:'servicios',component:ServiciosComponent},
-    {path:'detalle/:id',component:DetalleAutoComponent},
+    //{path:'detalle/:id',component:DetalleAutoComponent},
     {path:'register',component:RegisterComponent},
     {path:'financiamiento',component:FinanciamientoComponent},
     {path:'equipo',component:EquipoComponent},
--- src/app/auto.ts
+++ src/app/auto.ts
@@ -1,2 +1,16 @@
+
 export interface Auto {
+  id: number;
+  ofertas: boolean;
+  oportunidades: boolean;
+  marca: string;
+  modelo: string;
+  anio: number;
+  precio: number;
+  tipo_auto: string;
+  transmision: string;
+  kilometraje: number;
+  color: string;
+  pasajeros: number;
+  fuente_img: string[];
 }
