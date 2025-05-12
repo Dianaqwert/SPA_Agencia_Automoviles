7ec22bc21486938904e4b8f38e896927cf4756e0
Asus Ryzen 5 <esmeralda298rivera@gmail.com>
2025-05-11 20:23:46 -0600

    Commit : Cambios en el formualrio reactivo de servicios con  validaciones y local storage

---
+++ src/app/servicioReg.ts
@@ -0,0 +1,13 @@
+interface Registro{
+    id: number;
+    nombre: string;
+    username:string;
+    apellidos: string;
+    email: string;
+    direccion: string;
+    cp:number;
+    pais: string;
+    estado: string;
+    fechaCita: string
+    terminos:boolean;
+}
