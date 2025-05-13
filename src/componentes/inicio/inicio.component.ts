317206102dae91896eb4a613a808489e37730654
Starcrash16 <renerosendo8@gmail.com>
2025-05-12 20:06:58 -0600

    Version final, sin financiamiento

--- src/componentes/inicio/inicio.component.ts
+++ src/componentes/inicio/inicio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
-  standalone:true,
+  imports: [RouterModule],
+  standalone: true,
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
